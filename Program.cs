599acdd6c24e4710ad6c32040b543ecaabd4b508
Josh Winiberg <contact@joshwiniberg.co.uk>
2026-09-24 14:45:56 +0100

    My solution

--- Program.cs
+++ Program.cs
@@ -1,117 +1,253 @@
-﻿using System;
+﻿#region Global Variables
+// ----------------
+// GLOBAL VARIABLES
+// ----------------
+
+bool shouldExit = false;
+string exitReason = "";
 
 Random random = new Random();
 Console.CursorVisible = false;
 int height = Console.WindowHeight - 1;
 int width = Console.WindowWidth - 5;
-bool shouldExit = false;
 
 // Console position of the player
-int playerX = 0;
-int playerY = 0;
+(int x, int y) playerPosition = (0, 0);
+HashSet<(int x, int y)> playerHitBox = new();
+(int x, int y) lastPosition = (0, 0);
+HashSet<(int x, int y)> interpolatedPositions = new();
 
 // Console position of the food
-int foodX = 0;
-int foodY = 0;
+(int x, int y) foodPosition = (0, 0);
+HashSet<(int x, int y)> allFoodPieces = new();
 
 // Available player and food strings
-string[] states = {"('-')", "(^-^)", "(X_X)"};
-string[] foods = {"@@@@@", "$$$$$", "#####"};
+string[] states = { "('-')", "(^-^)", "(X_X)" };
+string[] foods = { "@@@@@", "$$$$$", "#####" };
 
 // Current player string displayed in the Console
 string player = states[0];
 
 // Index of the current food
 int food = 0;
+#endregion
+
+#region MainLoop
+// ---------
+// MAIN LOOP
+// ---------
 
 InitializeGame();
-while (!shouldExit) 
+while (!shouldExit)
 {
-    Move();
+    int speed = player == states[1] ? 4 : 1;
+
+    Move(speed, true);
+    TerminalResized();
+    SetHitbox();
+    CheckFoodCollision();
+    if (IsFoodGone())
+    {
+        ChangePlayer();
+        ShowFood();
+    }
+}
+Exit();
+#endregion
+
+#region Helpers
+// -------
+// HELPERS
+// -------
+
+void Exit()
+{
+    Console.Clear();
+    Console.Write("\x1b[3J");
+    Console.SetCursorPosition(0, 0);
+    Console.WriteLine($"{exitReason} Program exiting.");
 }
 
 // Returns true if the Terminal was resized 
-bool TerminalResized() 
+void TerminalResized()
 {
-    return height != Console.WindowHeight - 1 || width != Console.WindowWidth - 5;
+    bool wasResized = height != Console.WindowHeight - 1 || width != Console.WindowWidth - 5;
+
+    if (wasResized)
+    {
+        shouldExit = true;
+        if (exitReason == "")
+            exitReason = "Console was resized.";
+    }
 }
 
+void CheckFoodCollision()
+{
+    foreach (var box in playerHitBox)
+    {
+        if (allFoodPieces.Remove(box))
+        {
+            Console.SetCursorPosition(box.x, box.y);
+            Console.Write(" ");
+            Console.SetCursorPosition(playerPosition.x, playerPosition.y);
+            Console.Write(player);
+        }
+    }
+}
+
+bool IsFoodGone() => allFoodPieces.Count == 0;
+
 // Displays random food at a random location
-void ShowFood() 
+void ShowFood()
 {
     // Update food to a random index
     food = random.Next(0, foods.Length);
 
     // Update food position to a random location
-    foodX = random.Next(0, width - player.Length);
-    foodY = random.Next(0, height - 1);
+    bool overlaps;
+    do
+    {
+        overlaps = false;
+        foodPosition.x = random.Next(0, width - player.Length);
+        foodPosition.y = random.Next(0, height - 1);
+
+        for (int i = 0; i < foods[food].Length; i++)
+        {
+            if (playerHitBox.Contains((foodPosition.x + i, foodPosition.y)))
+            {
+                overlaps = true;
+                break;
+            }
+        }
+    }
+    while (overlaps);
+
+    if (playerHitBox.Count == 0)
+    {
+        if (exitReason == "")
+            exitReason = "Food generation error, player hitbox invalid.";
+        shouldExit = true;
+    }
 
     // Display the food at the location
-    Console.SetCursorPosition(foodX, foodY);
+    Console.SetCursorPosition(foodPosition.x, foodPosition.y);
     Console.Write(foods[food]);
+
+    for (int i = 0; i < foods[food].Length; i++)
+        allFoodPieces.Add((foodPosition.x + i, foodPosition.y));
 }
 
 // Changes the player to match the food consumed
-void ChangePlayer() 
+void ChangePlayer()
 {
     player = states[food];
-    Console.SetCursorPosition(playerX, playerY);
+    Console.SetCursorPosition(playerPosition.x, playerPosition.y);
     Console.Write(player);
+
+    if (player == states[2])
+        FreezePlayer();
 }
 
 // Temporarily stops the player from moving
-void FreezePlayer() 
+void FreezePlayer()
 {
-    System.Threading.Thread.Sleep(1000);
+    Thread.Sleep(1000);
     player = states[0];
 }
 
 // Reads directional input from the Console and moves the player
-void Move() 
+void Move(int speed, bool quitOnInvalidInput = false)
 {
-    int lastX = playerX;
-    int lastY = playerY;
-    
-    switch (Console.ReadKey(true).Key) 
+    interpolatedPositions.Clear();
+    lastPosition = playerPosition;
+
+    switch (Console.ReadKey(true).Key)
     {
         case ConsoleKey.UpArrow:
-            playerY--; 
+            for (int i = 0; i < speed; i++)
+            {
+                playerPosition.y--;
+                interpolatedPositions.Add(playerPosition);
+            }
             break;
-		case ConsoleKey.DownArrow: 
-            playerY++; 
+
+        case ConsoleKey.DownArrow:
+            for (int i = 0; i < speed; i++)
+            {
+                playerPosition.y++;
+                interpolatedPositions.Add(playerPosition);
+            }
             break;
-		case ConsoleKey.LeftArrow:  
-            playerX--; 
+
+        case ConsoleKey.LeftArrow:
+            for (int i = 0; i < speed; i++)
+            {
+                playerPosition.x--;
+                interpolatedPositions.Add(playerPosition);
+            }
             break;
-		case ConsoleKey.RightArrow: 
-            playerX++; 
+
+        case ConsoleKey.RightArrow:
+            for (int i = 0; i < speed; i++)
+            {
+                playerPosition.x++;
+                interpolatedPositions.Add(playerPosition);
+            }
             break;
-		case ConsoleKey.Escape:     
-            shouldExit = true; 
+
+        case ConsoleKey.Escape:
+            shouldExit = true;
+            if (exitReason == "")
+                exitReason = "User pressed escape.";
+            break;
+
+        default:
+            if (quitOnInvalidInput)
+            {
+                shouldExit = true;
+                if (exitReason == "")
+                    exitReason = "Invalid input.";
+            }
             break;
     }
 
     // Clear the characters at the previous position
-    Console.SetCursorPosition(lastX, lastY);
-    for (int i = 0; i < player.Length; i++) 
+    Console.SetCursorPosition(lastPosition.x, lastPosition.y);
+    for (int i = 0; i < player.Length; i++)
     {
         Console.Write(" ");
     }
 
     // Keep player position within the bounds of the Terminal window
-    playerX = (playerX < 0) ? 0 : (playerX >= width ? width : playerX);
-    playerY = (playerY < 0) ? 0 : (playerY >= height ? height : playerY);
+    playerPosition.x = (playerPosition.x < 0) ? 0 : (playerPosition.x >= width ? width : playerPosition.x);
+    playerPosition.y = (playerPosition.y < 0) ? 0 : (playerPosition.y >= height ? height : playerPosition.y);
 
     // Draw the player at the new location
-    Console.SetCursorPosition(playerX, playerY);
+    Console.SetCursorPosition(playerPosition.x, playerPosition.y);
     Console.Write(player);
 }
 
+void SetHitbox()
+{
+    playerHitBox.Clear();
+    for (int i = 0; i < player.Length; i++)
+        playerHitBox.Add((playerPosition.x + i, playerPosition.y));
+
+    foreach (var position in interpolatedPositions)
+    {
+        for (int i = 0; i < player.Length; i++)
+            playerHitBox.Add((position.x + i, position.y));
+    }
+}
+
 // Clears the console, displays the food and player
-void InitializeGame() 
+void InitializeGame()
 {
     Console.Clear();
+    for (int i = 0; i < 5; i++)
+        playerHitBox.Add((i, 0));
     ShowFood();
     Console.SetCursorPosition(0, 0);
     Console.Write(player);
 }
+#endregion
--- README.md
+++ README.md
@@ -1,3 +1,49 @@
-# Challenge-project-Create-methods-in-CSharp
+# Microsoft Learn Foundational C#: Create Methods in C# Console Applications
+## Challenge Project: Create a Mini-Game
 
-Starter and Final code for the Challenge project: "Create methods C# console applications" from the Microsoft Learn collection "Getting started with C#"
+## SUMMARY
+My solution to the mini-game challenge in Learn's Foundational C# course.<br>
+Initial commit contains the base code distributed for the assignment.<br>
+Branch: learnSolution contains the code given by Learn as an example of a valid solution.<br>
+Check history or compare branches to see my modifications and improvements.<br>
+
+## LICENSE
+Distributed under the MIT License.
+
+## ASSIGNMENT INSTRUCTIONS
+### Terminate on resize
+This feature must:<br>
+Determine if the terminal was resized before allowing the game to continue<br>
+Clear the Console and end the game if the terminal was resized<br>
+Display the following message before ending the program: Console was resized. Program exiting.<br>
+
+### Add optional termination
+Modify the existing Move method to support an optional parameter<br>
+If enabled, the optional parameter should detect nondirectional key input<br>
+If nondirectional input is detected, allow the game to terminate<br>
+
+### Check if the player consumed the food
+Create a method that uses the existing position variables of the player and food<br>
+The method should return a value<br>
+After the user moves the character, call your method to determine the following:<br>
+- Whether or not to use the existing method that changes player appearance<br>
+- Whether or not to use the existing method to redisplay the food<br>
+
+### Check if the player should freeze
+Create a method that checks if the current player appearance is (X_X)<br>
+The method should return a value<br>
+Before allowing the user to move the character, call your method to determine the following:<br>
+Whether or not to use the existing method that freezes character movement<br>
+Make sure the character is only frozen temporarily and the player can still move afterwards<br>
+Add an option to increase player speed<br>
+Modify the existing Move method to support an optional movement speed parameter<br>
+Use the parameter to increase or decrease right and left movement speed by 3<br>
+Create a method that checks if the current player appearance is (^-^)<br>
+The method should return a value<br>
+Call your method to determine if Move should use the movement speed parameter<br>
+
+## COMMENTS
+Besides implementing the above requirements:<br>
+    - I created two `HashSet<(int, int)>` tuples, one to store coordinates of all positions currently populated by the player, and the other to store the individual positions of each piece of food. Comparing both sets of coordinates allows any part of the player to interact with any individual piece of food. This system is also used to prevent food from spawning in any position populated by the player. The HashSets are cleared and reused to avoid runtime allocation. HashSets were preferred over arrays in case the developer wanted to change the length of the player or food icons: since the assignment was broken down into separate modules, I wanted something that wouldn't be broken by a change of spec in later modules. I also switch to using tuples to store index positions of food and player for consistency and to prevent possible mismatches.<br>
+    - `Move()` now increments positions `speed` number of times. This allows me to store every step in `interpolatedPositions`, which I add to the `playerHitbox` for collision detecting. This prevents the player tunneling through food.<br>
+    - I created an `exitReason` string that stores the reason for exiting the first time `shouldExit` is set to true. This is used to generate an exit log when the main loop is terminated.
